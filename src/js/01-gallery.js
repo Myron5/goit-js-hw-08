97f8f062c16c64bb2aa6872f18c7ab09e663bff4
unknown <myron.melnyk.kn.2021@lpnu.ua>
2022-12-26 00:46:32 +0200

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  images: document.querySelectorAll('.gallery__image'),
+  links: document.querySelectorAll('.gallery__link'),
+};
+
+refs.images.forEach((img, i) => {
+  img.setAttribute('src', galleryItems[i].preview);
+  img.setAttribute('js-idx', i);
+  img.setAttribute('alt', galleryItems[i].description);
+  img.parentNode.setAttribute('href', galleryItems[i].original);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simplelightbox', e => e.preventDefault());
+
+lightbox.on('error.simplelightbox', e => console.log(e));
