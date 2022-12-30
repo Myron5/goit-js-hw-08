e38d85d883f81b9dbc7b5bbd714f15a909796cc2
unknown <myron.melnyk.kn.2021@lpnu.ua>
2022-12-30 18:51:53 +0200

    tasks done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import trottle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const time = localStorage.getItem('videoplayer-current-time') || 0;
+player.setCurrentTime(time);
+
+player.on(
+  'timeupdate',
+  trottle(data => {
+    player
+      .getCurrentTime()
+      .then(seconds =>
+        localStorage.setItem(
+          'videoplayer-current-time',
+          JSON.stringify(seconds)
+        )
+      )
+      .catch(error => console.log(error));
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import trottle from 'lodash.throttle';
 
+const refs = {
+  formRef: document.querySelector('.feedback-form'),
+  emailInputRef: document.querySelector('.feedback-email'),
+  messageInputRef: document.querySelector('.feedback-message'),
+  buttonRef: document.querySelector('.feedback-button'),
+};
+
+(() => {
+  const getInput = JSON.parse(localStorage.getItem('input')) || {
+    email: '',
+    message: '',
+  };
+  refs.emailInputRef.value = getInput.email;
+  refs.messageInputRef = getInput.message;
+})();
+
+refs.formRef.addEventListener(
+  'input',
+  trottle(
+    e =>
+      localStorage.setItem(
+        'input',
+        JSON.stringify({
+          email: e.currentTarget.elements.email.value,
+          message: e.currentTarget.elements.message.value,
+        })
+      ),
+    500
+  )
+);
+
+refs.formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  console.log(email.value, message.value);
+  e.currentTarget.reset();
+  localStorage.clear();
+});
